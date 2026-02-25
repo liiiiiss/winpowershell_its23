de95d30e9cee8d2ae4051e0b8eb555807fabb538
liis-mirell Palmiste <liis-mirell.palmiste@voco.ee>
2026-02-25 08:49:34 +0200

    harjutus2

---
+++ harjutus2hashtables.ps1
@@ -0,0 +1,14 @@
+﻿# 5. Hashtable
+
+$student = @{
+    Name = "Liis"
+    Age = 18
+    Course = "IT-spetsialist"
+    School = "voco"
+}
+
+Write-Host "Student Information:" -ForegroundColor Cyan
+Write-Host "Name: $($student.Name)" -ForegroundColor Yellow
+Write-Host "Age: $($student.Age)" -ForegroundColor Yellow
+Write-Host "Course: $($student.Course)" -ForegroundColor Yellow
+Write-Host "School: $($student.School)" -ForegroundColor Yellow
