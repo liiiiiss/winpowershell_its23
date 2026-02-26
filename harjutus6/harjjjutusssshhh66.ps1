dae3ece530dceab42c9197cd668bb736bbcccfc4
liis-mirell Palmiste <liis-mirell.palmiste@voco.ee>
2026-02-26 21:08:42 +0200

    harjutus6

---
+++ harjutus6/harjjjutusssshhh66.ps1
@@ -0,0 +1,37 @@
+﻿
+# Practice 6 - Loops 
+
+
+
+$students = Import-Csv "C:\Temp\students.csv"
+
+
+$result = @()
+
+
+foreach ($s in $students) {
+
+    $schoolType = ""
+
+    if ($s.Age -ge 4 -and $s.Age -le 10) {
+        $schoolType = "Junior"
+    }
+    elseif ($s.Age -ge 11 -and $s.Age -le 17) {
+        $schoolType = "Senior"
+    }
+
+    $obj = [PSCustomObject]@{
+        Name   = $s.Name
+        School = $schoolType
+    }
+
+    $result += $obj
+}
+
+
+$result | Format-Table -AutoSize
+
+
+$result | Export-Csv "C:\Temp\SchoolResult.csv" -NoTypeInformation
+
+Write-Host "`nResult exported to C:\Temp\SchoolResult.csv" -ForegroundColor Green
