b52f75330d6669e695c8717f8dc883d94f8569de
liis-mirell Palmiste <liis-mirell.palmiste@voco.ee>
2026-02-26 21:44:06 +0200

    harjutus10

---
+++ harjutus10.ps1
@@ -0,0 +1,22 @@
+﻿
+# Practice 10 – Do Until loop
+
+
+Write-Host "Open 3 Notepad windows now..." -ForegroundColor Yellow
+Start-Sleep -Seconds 5
+
+$counter = 0
+
+Do {
+    $notepads = Get-Process -Name notepad -ErrorAction SilentlyContinue
+
+    if ($notepads.Count -gt 0) {
+        Write-Host "Notepad is running" -ForegroundColor Green
+        $counter++
+        Start-Sleep -Seconds 1
+    }
+
+} Until ((Get-Process -Name notepad -ErrorAction SilentlyContinue).Count -eq 0)
+
+Write-Host "All Notepad windows are closed." -ForegroundColor Red
+Write-Host "The message was displayed $counter times." -ForegroundColor Cyan
