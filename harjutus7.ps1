31f397dcc208857e989f5115efb6807d34837434
liis-mirell Palmiste <liis-mirell.palmiste@voco.ee>
2026-02-26 21:16:31 +0200

    harjutus 7

---
+++ harjutus7.ps1
@@ -0,0 +1,34 @@
+﻿
+# Harjutus 7
+
+
+
+$groups = @("Red", "Green", "Yellow", "Blue")
+
+
+$result = @()
+
+
+for ($i = 1; $i -le 20; $i++) {
+
+    
+    $randomGroup = Get-Random -InputObject $groups
+
+    
+    $obj = [PSCustomObject]@{
+        RollNumber = $i
+        Group      = $randomGroup
+    }
+
+    $result += $obj
+}
+
+
+$result | Format-Table -AutoSize
+
+$counter = 1
+
+while ($counter -le 5) {
+    Write-Host "Current value: $counter" -ForegroundColor Cyan
+    $counter++
+    }
