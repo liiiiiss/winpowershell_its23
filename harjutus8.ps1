9025d37af219e644b5d33c439c7cd53c24593dfe
liis-mirell Palmiste <liis-mirell.palmiste@voco.ee>
2026-02-26 21:34:01 +0200

    harjutus8

---
+++ harjutus8.ps1
@@ -0,0 +1,19 @@
+﻿
+# Practice 8 – Monitor Notepad using Do-While loop
+
+
+Write-Host "Open 3 Notepad windows now..." -ForegroundColor Yellow
+Start-Sleep -Seconds 5
+
+Do {
+
+    $notepads = Get-Process -Name notepad -ErrorAction SilentlyContinue
+
+    if ($notepads.Count -gt 0) {
+        Write-Host "Notepad is running... ($($notepads.Count) open)" -ForegroundColor Green
+        Start-Sleep -Seconds 2
+    }
+
+} while ((Get-Process -Name notepad -ErrorAction SilentlyContinue).Count -gt 0)
+
+Write-Host "All Notepad windows are closed. Loop ended." -ForegroundColor Red
