c3d902de360352c95f8cebf48f48098530e7ace5
liis-mirell Palmiste <liis-mirell.palmiste@voco.ee>
2026-03-02 21:10:06 +0200

    harjutus12

---
+++ harjutus12.ps1
@@ -0,0 +1,15 @@
+﻿# --------------------------------------------------
+# Practice 12 – Function WITHOUT parameter
+# --------------------------------------------------
+
+function Get-ServiceCount
+{
+    $running = (Get-Service | Where-Object { $_.Status -eq "Running" }).Count
+    $stopped = (Get-Service | Where-Object { $_.Status -eq "Stopped" }).Count
+
+    Write-Host "Running services count: $running" -ForegroundColor Green
+    Write-Host "Stopped services count: $stopped" -ForegroundColor Red
+}
+
+# Call function
+Get-ServiceCount
